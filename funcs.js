133f5ae5fb5d698e325393b723c677a170af11ac
sharifloo-net <sharifloo-net@gmail.com>
2024-03-14 17:59:15 +0330

    isssue testing - #1

--- funcs.js
+++ funcs.js
@@ -5,7 +5,5 @@ const getMe = (user) => {
 const userAuth = (user) => {
     if (user.name == 'amin' && user.password == '1234') {
         return true;
-    } else {
-        return false;
-    }
+    } else return false;
 };
